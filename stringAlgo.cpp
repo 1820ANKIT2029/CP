981814a3b470f8f57da1990a74eea54035fca790
Ankit Kumar <ankitkumar00002250908@gmail.com>
2026-05-25 03:41:28 +0000

    Add Suffix Array implementation and related algorithms for efficient string processing

--- stringAlgo.cpp
+++ stringAlgo.cpp
@@ -37,7 +37,6 @@ vector<int> kmp_search(const string& pattern, const string& text) {
     return occurrences;
 }
 
-
 // Z-algorithm for pattern matching
 vector<int> z_function(const string& s) {
     int n = s.length();
@@ -304,5 +303,63 @@ struct AhoCorasick {
             }
         }
         return matches;
+    }
+};
+
+struct SuffixArray {
+    string s;
+    int n;
+    vector<int> p, c, lcp;
+
+    // O(N log N) Construction
+    SuffixArray(string text) {
+        s = text + "$"; 
+        n = s.length();
+        p.assign(n, 0);
+        c.assign(n, 0);
+        build_sa();
+        build_lcp();
+    }
+
+    void build_sa() {
+        vector<pair<char, int>> a(n);
+        for (int i = 0; i < n; i++) a[i] = {s[i], i};
+        sort(a.begin(), a.end());
+
+        for (int i = 0; i < n; i++) p[i] = a[i].second;
+        c[p[0]] = 0;
+        for (int i = 1; i < n; i++) {
+            c[p[i]] = c[p[i - 1]] + (a[i].first != a[i - 1].first);
+        }
+
+        int k = 0;
+        while ((1 << k) < n) {
+            vector<pair<pair<int, int>, int>> b(n);
+            for (int i = 0; i < n; i++) {
+                b[i] = {{c[i], c[(i + (1 << k)) % n]}, i};
+            }
+            sort(b.begin(), b.end());
+
+            for (int i = 0; i < n; i++) p[i] = b[i].second;
+            c[p[0]] = 0;
+            for (int i = 1; i < n; i++) {
+                c[p[i]] = c[p[i - 1]] + (b[i].first != b[i - 1].first);
+            }
+            k++;
+        }
+    }
+
+    // Kasai's Algorithm for LCP in O(N)
+    void build_lcp() {
+        lcp.assign(n, 0);
+        int k = 0;
+        for (int i = 0; i < n - 1; i++) {
+            int pi = c[i];
+            int j = p[pi - 1];
+            // LCP of suffix i and j
+            while (s[i + k] == s[j + k]) k++;
+            lcp[pi] = k;
+            if (k > 0) k--;
+        }
     }
 };
